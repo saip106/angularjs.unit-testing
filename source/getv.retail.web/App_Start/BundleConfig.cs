07a2a89e32bae75d4ef8b240bdb54146a76d7a9e
Brandon Young <Brandon.Young@jhm.org>
2014-01-02 13:50:55 -0600

    fixed build issues.  fixed dependency of bootstrap (pointed to the correct nuget)

--- source/getv.retail.web/App_Start/BundleConfig.cs
+++ source/getv.retail.web/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Getv.Retail.Web
 {
@@ -20,7 +19,7 @@ namespace Getv.Retail.Web
 					  "~/Scripts/bootstrap.js",
 					  "~/Scripts/respond.js"));
 
-			bundles.Add(new StyleBundle("~/Content/css").Include(
+			bundles.Add(new StyleBundle("~/bundles/bootstrap").Include(
 					  "~/Content/bootstrap.css",
 					  "~/Content/site.css"));
 		}
