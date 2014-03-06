4859bfd820b714b0884a690bdb988a14a28b8e21
Sai Gudigundla <saip106@msn.com>
2014-03-06 18:40:35 -0500

    removed all old stuff

--- source/getv.retail.web/Scripts/_references.js
+++
@@ -1,5 +0,0 @@
-﻿/// <reference path="jquery-1.9.1.js" />
-/// <autosync enabled="true" />
-/// <reference path="modernizr-2.6.2.js" />
-/// <reference path="bootstrap.js" />
-/// <reference path="respond.js" />
