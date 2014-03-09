4d197ced50a587aed2caec46df807689534a8237
Sai Gudigundla <saip106@msn.com>
2014-03-09 13:45:19 -0400

    changed the menus to meaningful names

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,22 @@
 'use strict';
 
 angular.module('getvretailwebApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/dahsboard.html',
+                controller: 'SellCtrl'
+            })
+            .when('/sell', {
+                templateUrl: 'views/sell.html',
+                controller: 'SellCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('getvretailwebApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('SellCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
