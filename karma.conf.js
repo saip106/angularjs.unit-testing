d2edcef93be7ed3c4b1d50ae3c7eb588ca5f6d3c
Sai Gudigundla <saip106@msn.com>
2014-03-17 09:54:07 -0400

    fixed test failures by adding required references to karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -17,6 +17,9 @@ module.exports = function(config) {
       'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-ui-router/release/angular-ui-router.js',
+      'app/bower_components/jquery/jquery.js',
+      'app/bower_components/bootstrap/dist/js/bootstrap.js',
+      'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
       'test/spec/**/*.js'
