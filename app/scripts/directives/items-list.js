505bd972e65383f62eb8d3971a6c68ea5615e5d7
Sai Gudigundla <saip106@msn.com>
2014-03-21 00:16:39 -0400

    added list items directive

---
+++ app/scripts/directives/items-list.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('getvApp')
+    .directive('itemsList', function ($compile) {
+        return {
+            restrict: 'A',
+            scope: {
+                itemsListData: '='
+            },
+            templateUrl: 'templates/items-list.tmpl.html',
+            link: function (scope, element, attrs) {
+
+                scope.removeItem = function (index) {
+                    scope.itemsListData.splice(index, 1);
+                };
+
+            }
+        };
+    });
