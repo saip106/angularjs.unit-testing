daa7d53030b1c3bde084a0c2dc91e9cf955ab971
Sai Gudigundla <saip106@msn.com>
2014-04-25 18:11:36 -0400

    added loading dialog when creating order and adding items to order

--- app/scripts/sell/items-search-service.js
+++ app/scripts/sell/items-search-service.js
@@ -1,14 +1,15 @@
 'use strict';
 
 angular.module('getvApp')
-    .factory('ItemsSearchService', ['$http', 'SessionStorageService',
-        function ($http, Session) {
+    .factory('ItemsSearchService', ['$http', 'SessionStorageService', '$log',
+        function ($http, Session, $log) {
 
             var userSession = Session.get('userSession'),
                 authorizationHeader = 'Bearer ' + userSession.access_token;
 
             return {
                 searchItems : function (searchText) {
+
                     return $http({
                         method : 'GET',
                         url : 'https://v1-dev-retail-api.jhm.info/stockitems?$top=10&$filter=substringof(\'' + searchText + '\', Id) or substringof(\'' + searchText + '\', Name) or substringof(\'' + searchText + '\', Description)',
@@ -17,7 +18,7 @@ angular.module('getvApp')
                         }
                     })
                         .then(function (result) {
-                            console.log(result.data.items);
+                            $log.debug(result.data.items);
                             return result.data.items;
                         }
                     );
