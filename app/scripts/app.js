dc5b2859fe36160faaaf69905086057f024d5f66
Sai Gudigundla <saip106@msn.com>
2014-06-08 22:51:52 -0400

    cleanup of routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,8 +10,8 @@ var getvApp = angular.module('getvApp',
         'underscore'
     ]);
 
-getvApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
-    function ($stateProvider, $urlRouterProvider, $locationProvider) {
+getvApp.config(['$stateProvider', '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
 
         $urlRouterProvider.otherwise('login');
 
@@ -76,45 +76,7 @@ getvApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
                 url : 'history',
                 templateUrl : 'views/history.html'
             });
-
-        // FIX for trailing slashes. Gracefully "borrowed" from https://github.com/angular-ui/ui-router/issues/50
-        $urlRouterProvider.rule(function ($injector, $location) {
-            if($location.protocol() === 'file')
-                return;
-
-            var path = $location.path()
-            // Note: misnomer. This returns a query object, not a search string
-                , search = $location.search()
-                , params
-                ;
-
-            // check to see if the path already ends in '/'
-            if(path[path.length - 1] === '/') {
-                return;
-            }
-
-            // If there was no search string / query params, return with a `/`
-            if(Object.keys(search).length === 0) {
-                return path + '/';
-            }
-
-            // Otherwise build the search string and return a `/?` prefix
-            params = [];
-            angular.forEach(search, function (v, k) {
-                params.push(k + '=' + v);
-            });
-            return path + '/?' + params.join('&');
-        });
     }]);
-
-getvApp.constant('AUTH_EVENTS', {
-    loginSuccess : 'auth-login-success',
-    loginFailed : 'auth-login-failed',
-    logoutSuccess : 'auth-logout-success',
-    sessionTimeout : 'auth-session-timeout',
-    notAuthenticated : 'auth-not-authenticated',
-    notAuthorized : 'auth-not-authorized'
-});
 
 getvApp.run(function ($rootScope, $log) {
 
@@ -154,4 +116,4 @@ getvApp.run(function ($rootScope, $log) {
             $log.debug("View Load: the view is loaded, and DOM rendered!");
         });
 
-})
+});
