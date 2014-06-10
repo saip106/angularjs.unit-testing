187bdf251680070c64fac158b2dd590ad407ec51
Sai Gudigundla <saip106@msn.com>
2014-06-09 22:08:29 -0400

    changed gruntfile to not cache pngs

--- Gruntfile.js
+++ Gruntfile.js
@@ -159,7 +159,7 @@ module.exports = function (grunt) {
                     src : [
                         '<%= yeoman.dist %>/scripts/{,*/}*.js',
                         '<%= yeoman.dist %>/styles/{,*/}*.css',
-                        '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+                        '<%= yeoman.dist %>/images/{,*/}*.{jpg,jpeg,gif,webp,svg}',
                         '<%= yeoman.dist %>/styles/fonts/*'
                     ]
                 }
