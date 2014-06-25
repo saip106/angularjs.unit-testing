cd2368d8e2c019342b3ffa4e21a0e4f888eb2e9f
Sai Gudigundla <saip106@msn.com>
2014-06-25 19:59:45 -0400

    added products.json for quick-keys, got pagination working !!!

--- app/scripts/sell/quick-keys-panel-directive.js
+++ app/scripts/sell/quick-keys-panel-directive.js
@@ -1,25 +1,30 @@
 'use strict';
 
 angular.module('getvApp')
-    .directive('quickKeysPanel', ['$log', '_', function ($log, _) {
-        return {
-            replace : true,
-            scope : {},
-            restrict : "A",
-	        templateUrl : 'templates/quick-keys-panel-template.html',
-            link : function (scope, element, attrs) {
-				scope.currentPage = 1;
-	            scope.totalItems = 100;
-	            scope.pageChanged = function () {
-		            $log.debug('page changed...');
-	            };
+    .directive('quickKeysPanel', ['$http', '$log', 'QuickKeysRetriever',
+        function ($http, $log, QuickKeysRetriever) {
+            return {
+                replace : true,
+                scope : {},
+                restrict : "A",
+                templateUrl : 'templates/quick-keys-panel-template.html',
+                link : function (scope, element, attrs) {
 
-	            scope.items = [];
-	            _.each(_.range(10), function (index) {
-		            scope.items.push({
-			            name: "Product Name - " + index
-		            });
-	            });
-            }
-        };
-    }]);
+                    var itemsPerPage = 8;
+                    scope.currentPage = 1;
+
+                    QuickKeysRetriever
+                        .retrieve(scope.currentPage)
+                        .then(function (data) {
+                            scope.items = data.items;
+                            scope.totalItems = data.items.length;
+                            scope.currentPageItems = data.items.slice(0, itemsPerPage);
+                        });
+
+                    scope.pageChanged = function () {
+                        var start = itemsPerPage * (scope.currentPage - 1);
+                        scope.currentPageItems = scope.items.slice(start, start + itemsPerPage);
+                    };
+                }
+            };
+        }]);
--- app/scripts/sell/quick-keys-retriever.js
+++ app/scripts/sell/quick-keys-retriever.js
@@ -1,26 +1,24 @@
 'use strict';
 
 angular.module('getvApp')
-    .factory('ItemsSearchService', ['$http', 'SessionStorageService', '$log',
+    .factory('QuickKeysRetriever', ['$http', 'SessionStorageService', '$log',
         function ($http, Session, $log) {
 
             var userSession = Session.get('userSession'),
                 authorizationHeader = 'Bearer ' + userSession.access_token;
 
             return {
-                searchItems : function (searchText) {
+                retrieve : function (currentPage) {
 
                     return $http({
                         method : 'GET',
-                        url : 'https://v1-dev-retail-api.jhm.info/stockitems?$top=10&$filter=substringof(\'' + searchText + '\', Id) or substringof(\'' + searchText + '\', Name)',
+                        url : 'data/products.json',
                         headers : {
                             authorization : authorizationHeader
                         }
                     })
                         .then(function (result) {
-                            $log.debug(result.data.items);
-                            window.xxx = result.data.items;
-                            return result.data.items;
+                            return result.data;
                         }
                     );
                 }
