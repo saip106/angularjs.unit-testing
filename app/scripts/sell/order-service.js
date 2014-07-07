bc8e483a5b760b90b5c60cedf432e25d76a9ba46
Sai Gudigundla <saip106@msn.com>
2014-07-07 07:15:03 -0400

    added logic to check and see if the item already exists before adding it

--- app/scripts/sell/order-service.js
+++ app/scripts/sell/order-service.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('getvApp')
-    .factory('OrderService', ['$http', 'LoadingDialogService', '$log',
-        function ($http, LoadingDialogService, $log) {
+    .factory('OrderService', ['$http', 'LoadingDialogService', '$log', '_',
+        function ($http, LoadingDialogService, $log, _) {
 
             var createNewOrder = function (order, authorizationHeader) {
 
@@ -30,38 +30,78 @@ angular.module('getvApp')
 
                 LoadingDialogService.open('Adding item');
 
-                $http({
-                    method : 'POST',
-                    url : 'https://v1-dev-retail-api.jhm.info/orders/' + order.orderId + '/items/' + selectedItem.id,
-                    headers : {
-                        authorization : authorizationHeader
-                    },
-                    data : {
-                        "quantity" : 1
-                    }
-                })
-                    .success(function (result) {
-                        $log.debug('item with id ' + selectedItem.id + ' is successfully added to order ' + order.orderId);
-                        LoadingDialogService.close();
-                    })
-                    .error(function (error) {
-                        $log.error(error);
-                        LoadingDialogService.close();
-                    });
+	            var doesOrderContainsThisItem = _.any(order.items, function (item) {
+		            return item.id === selectedItem.id;
+	            });
 
-                order.items.push({
-                    id : selectedItem.id,
-                    name : selectedItem.name,
-                    description : selectedItem.description,
-                    unitPrice : selectedItem.listPrice,
-                    quantity : 1,
-                    total : selectedItem.listPrice,
-                    format : selectedItem.format,
-                    isOnSale : selectedItem.isOnSale ? "YES" : "NO",
-                    product : selectedItem.product,
-                    stockItemWarehouses : selectedItem.stockItemWarehouses
-                });
+	            if (doesOrderContainsThisItem) {
+		            addExistingItem(selectedItem, order, authorizationHeader);
+	            }
+	            else {
+		            addNewItem(selectedItem, order, authorizationHeader);
+	            }
             };
+
+	        var addNewItem = function (selectedItem, order, authorizationHeader) {
+		        $http({
+			        method : 'POST',
+			        url : 'https://v1-dev-retail-api.jhm.info/orders/' + order.orderId + '/items/' + selectedItem.id,
+			        headers : {
+				        authorization : authorizationHeader
+			        },
+			        data : {
+				        "quantity" : 1
+			        }
+		        })
+			        .success(function (result) {
+				        $log.debug('item with id ' + selectedItem.id + ' is successfully added to order ' + order.orderId);
+				        LoadingDialogService.close();
+			        })
+			        .error(function (error) {
+				        $log.error(error);
+				        LoadingDialogService.close();
+			        });
+
+		        order.items.push({
+			        id : selectedItem.id,
+			        name : selectedItem.name,
+			        description : selectedItem.description,
+			        unitPrice : selectedItem.listPrice,
+			        quantity : 1,
+			        total : selectedItem.listPrice,
+			        format : selectedItem.format,
+			        isOnSale : selectedItem.isOnSale ? "YES" : "NO",
+			        product : selectedItem.product,
+			        stockItemWarehouses : selectedItem.stockItemWarehouses
+		        });
+	        }
+
+	        var addExistingItem = function (selectedItem, order, authorizationHeader) {
+
+		        var existingItem = _.find(order.items, function (item) {
+					return item.id === selectedItem.id;
+		        });
+
+		        $http({
+			        method : 'PATCH',
+			        url : 'https://v1-dev-retail-api.jhm.info/orders/' + order.orderId + '/items/' + selectedItem.id,
+			        headers : {
+				        authorization : authorizationHeader
+			        },
+			        data : {
+				        "quantity" : existingItem.quantity + 1
+			        }
+		        })
+			        .success(function (result) {
+				        $log.debug('item with id ' + selectedItem.id + ' is successfully added to order ' + order.orderId);
+				        existingItem.quantity += 1;
+				        LoadingDialogService.close();
+			        })
+			        .error(function (error) {
+				        $log.error(error);
+				        LoadingDialogService.close();
+			        });
+	        }
 
             var deleteOrder = function (orderId, authorizationHeader) {
 
